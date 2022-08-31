7cddd930f3ae99c380f3991a07ebe5deeabac1e7
Monk <kenjiyamane0@gmail.com>
2022-08-31 20:08:58 -0300

    remove blank line

--- proj01/src/polygon.js
+++ proj01/src/polygon.js
@@ -3,7 +3,6 @@ import {
 } from './math.js'
 
 export function pointInPolygon(point, edges) {
-
     let intersections = 0
     for (const edge of edges) {
         if (segmentsIntersect([...point, 1], [...point, 0], [...edge.first, 1], [...edge.second, 1])) {
